565f2479e1846ca4e661053dc7adc7564817f539
Gagani Amarathunga <gagani.amarathunga@gmail.com>
2018-08-10 19:57:28 +0530

    Rearrange code

--- HorizontalListView/app/src/main/java/com/android/horizontallistview/data/DataUtils.java
+++ HorizontalListView/app/src/main/java/com/android/horizontallistview/data/DataUtils.java
@@ -5,19 +5,53 @@ import com.android.horizontallistview.R;
 public class DataUtils {
 
     // Item names
-    private static String fruitItemNames[] = {"Apples", "Mandarins", "Grapes", "Cherries", "Plums", "Persimmons", "Currants"};
-    private static String vegetableItemNames[] = {"Carrot", "Green Beans", "Tomatoes", "Broccoli", "Pumpkin", "Potatoes", "Asparagus"};
+    private static String fruitItemNames[] = {
+            "Apples",
+            "Mandarins",
+            "Grapes",
+            "Cherries",
+            "Plums",
+            "Persimmons",
+            "Currants"};
+
+    private static String vegetableItemNames[] = {
+            "Carrot",
+            "Green Beans",
+            "Tomatoes",
+            "Broccoli",
+            "Pumpkin",
+            "Potatoes",
+            "Asparagus"};
 
     // Image IDs of the items
-    private static int fruitImages[] = {R.drawable.apples, R.drawable.mandarins, R.drawable.grapes, R.drawable.cherries, R.drawable.plums, R.drawable.persimmons, R.drawable.currants};
-    private static int vegetableImages[] = {R.drawable.carrot, R.drawable.green_beans, R.drawable.tomatoes, R.drawable.broccoli, R.drawable.pumpkin, R.drawable.potatos, R.drawable.asparagus};
+    private static int fruitImages[] = {
+            R.drawable.apples,
+            R.drawable.mandarins,
+            R.drawable.grapes,
+            R.drawable.cherries,
+            R.drawable.plums,
+            R.drawable.persimmons,
+            R.drawable.currants};
+
+    private static int vegetableImages[] = {
+            R.drawable.carrot,
+            R.drawable.green_beans,
+            R.drawable.tomatoes,
+            R.drawable.broccoli,
+            R.drawable.pumpkin,
+            R.drawable.potatos,
+            R.drawable.asparagus};
+
+    // Prices of the items
+    private static int fruitPrices[] = {2, 5, 7, 8, 3, 2, 4};
+    private static int vegetablePrices[] = {3, 2, 4, 7, 5, 2, 4};
 
     public String[] getFruitItemNames() {
         return fruitItemNames;
     }
 
     public void setFruitItemNames(String[] fruitItemNames) {
-        this.fruitItemNames = fruitItemNames;
+        DataUtils.fruitItemNames = fruitItemNames;
     }
 
     public String[] getVegetableItemNames() {
@@ -25,7 +59,7 @@ public class DataUtils {
     }
 
     public void setVegetableItemNames(String[] vegetableItemNames) {
-        this.vegetableItemNames = vegetableItemNames;
+        DataUtils.vegetableItemNames = vegetableItemNames;
     }
 
     public int[] getFruitImages() {
@@ -33,7 +67,7 @@ public class DataUtils {
     }
 
     public void setFruitImages(int[] fruitImages) {
-        this.fruitImages = fruitImages;
+        DataUtils.fruitImages = fruitImages;
     }
 
     public int[] getVegetableImages() {
@@ -41,7 +75,22 @@ public class DataUtils {
     }
 
     public void setVegetableImages(int[] vegetableImasge) {
-        this.vegetableImages = vegetableImasge;
+        vegetableImages = vegetableImasge;
     }
 
+    public int[] getFruitPrices() {
+        return fruitPrices;
+    }
+
+    public void setFruitPrices(int[] fruitPrices) {
+        DataUtils.fruitPrices = fruitPrices;
+    }
+
+    public int[] getVegetablePrices() {
+        return vegetablePrices;
+    }
+
+    public void setVegetablePrices(int[] vegetablePrices) {
+        DataUtils.vegetablePrices = vegetablePrices;
+    }
 }
